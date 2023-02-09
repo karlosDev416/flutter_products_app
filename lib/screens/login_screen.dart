79136bd3874cf61d7b6e2078d3d734b4fd30156b
Karlos Aguirre <karlos.dev416@gmail.com>
2023-02-09 15:46:03 -0600

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_products_app/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground(),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_products_app/widgets/auth_background.dart';
